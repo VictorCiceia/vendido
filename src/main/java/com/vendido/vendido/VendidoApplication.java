c2d16590d805c49aaf78d4c654afbb2c75b0065a
VictorCiceia <victormciceia@gmail.com>
2021-11-04 23:55:08 -0300

    Creacion del proyecto de spring boot

---
+++ src/main/java/com/vendido/vendido/VendidoApplication.java
@@ -0,0 +1,13 @@
+package com.vendido.vendido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendidoApplication.class, args);
+	}
+
+}
